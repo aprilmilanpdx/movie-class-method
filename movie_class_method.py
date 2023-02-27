bccab0e9d85e52d8cc529d684faa4558ec8fd080
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2023-02-26 20:57:46 -0800

    final commit

--- movie_class_method.py
+++ movie_class_method.py
@@ -1,10 +1,13 @@
-# We've already defined a movie class like this:
 class Movie:
     def __init__(self, new_name, new_director):
         self.name = new_name
         self.director = new_director
 
-    # let's try to add a method `print_info()` here:
+    def print_info(self):
+        print(f"<<{self.name}>> by {self.director}")
 
+# <<movie_name>> by movie_director
+# The movie_name and movie_director should be replaced by the according properties inside the object that calls the print_info() method.
 
-# You only need to finish the method, we will take care of the object creation and call your method for you!
+my_movie = Movie('The Matrix', 'Wachowski')
+my_movie.print_info()
